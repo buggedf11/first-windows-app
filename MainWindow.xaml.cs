e99845913d9e864fbae0caf939b2fe888e54bdb5
buggedf11 <120758948+buggedf11@users.noreply.github.com>
2025-12-16 16:03:39 +0000

    Add missing using directives for WPF controls and shapes in MainWindow

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Diagnostics;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Media;
+using System.Windows.Shapes;
 
 namespace ServerLauncher
 {
